96b70226bc480af9cacef151a3d9e88617ac1178
John Cape <139086976+Donnerpartyplatter@users.noreply.github.com>
2024-12-05 14:15:55 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import pyodbc
+
+app = Flask(__name__)
+
+# SQL Server Connection
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=sqlservergpmtwa43hxkby.database.windows.net;'
+    'DATABASE=sampledb;'
+    'UID=useradmin;'
+    'PWD=Passw0rd!!!!'
+)
+cursor = conn.cursor()
+
+# Home Page with Form
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        email = request.form['email']
+        school = request.form['school']
+
+        # Insert data into SQL Server
+        cursor.execute(
+            "INSERT INTO Students (Name, PhoneNumber, Email, School) VALUES (?, ?, ?, ?)",
+            (name, phone, email, school)
+        )
+        conn.commit()
+        return "Student information submitted successfully!"
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
